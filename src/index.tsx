8716b0a11981857a049119af39f7ce6be3f6ac92
Katja Weimer <katjawmr92@gmail.com>
2024-09-05 15:52:40 +0200

    add

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+//import './App';
+import reportWebVitals from './reportWebVitals';
+//import App from '../App';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <React.StrictMode>
+  <div>
+    <h1>hi</h1>
+  </div>
+  </React.StrictMode>
+);
+
+
+reportWebVitals();
