92ab368bb037e46b81910bb0b3a4b3216205677d
Katja Weimer <katjawmr92@gmail.com>
2024-09-05 16:58:44 +0200

    header

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,9 @@
+import styles from './header.module.css'
+
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className={styles.header}>Header</header>
+  )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 //import './App';
 import reportWebVitals from './reportWebVitals';
+import Header from './components/header/Header.tsx';
 //import App from '../App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
+    <><Header/></>
   <div>
     <h1>hi</h1>
   </div>
