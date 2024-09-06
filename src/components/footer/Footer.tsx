8683c5c0f6d3399f3cae83c0bbb5e67decebfa51
wwtraveller <ashilimova@mail.ru>
2024-09-06 13:47:00 +0200

    new version 06.09

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './footer.module.css'
+
+
+export default function Footer() {
+    return (
+        <>
+        <footer className={styles.footer}>
+            footer
+        </footer>
+
+        </>
+    )
+}
