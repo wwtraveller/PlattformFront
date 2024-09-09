f5e274c69313b1350b970f0f987346eadf81ce85
wwtraveller <ashilimova@mail.ru>
2024-09-09 17:56:05 +0200

    add new styles for header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,8 +2,9 @@ import { Link, useLocation } from 'react-router-dom';
 import styles from './header.module.css';
 
 import { links } from './links';
-import Button from '../button/Button';
+// import Button from '../button/Button';
 import { useState } from 'react';
+import Button from '../button/Button';
 
 
 export default function Header() {
@@ -15,11 +16,20 @@ export default function Header() {
   setSearchQuery(event.target.value);
   };
 
-  // Функция, которая будет вызвана при нажатии на кнопку поиска
-  const handleSearch = () => {
-  console.log(`Поиск по запросу: ${searchQuery}`);
-  // Здесь можно добавить логику для выполнения поиска, например, сделать запрос к API
+  // Функция запуска поиска по нажатию Enter
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      console.log(`Поиск по запросу: ${searchQuery}`);
+      // Добавить логику для выполнения поиска, запрос к API
+      setSearchQuery(''); // Очистка поля поиска после запуска
+    }
   };
+
+  // Функция, которая будет вызвана при нажатии на кнопку поиска
+  // const handleSearch = () => {
+  // console.log(`Поиск по запросу: ${searchQuery}`);
+  // Здесь можно добавить логику для выполнения поиска, например, сделать запрос к API
+  //};
 
   return (
     <header className={styles.header}>
@@ -27,21 +37,22 @@ export default function Header() {
       {links.map((el, index) => (
         <Link
           key={index}
-          className={location.pathname === el.pathname ? styles.active : ''}
+          className={`${styles.navLink} ${
+            location.pathname === el.pathname ? styles.active : ''}`}
           to={el.pathname}>{el.title}</Link>
       ))}
       </div>
       <div className={styles.navLeft}>
-        {/* Блок для поиска */}
+        {/* Блок для поиска без кнопки */}
         <div className={styles.navSearch}>
           <input
             type="text"
             value={searchQuery}
             onChange={handleInputChange}
+            onKeyPress={handleKeyPress} // Обработчик нажатия Enter
             placeholder="Введите запрос"
             className={styles.searchInput} // Добавьте стили по желанию
           />
-          <Button type="button" name="Поиск" onClick={handleSearch} />
         </div>
         
         <Link to={'/login'} className={styles.loginButton}><Button name='Войти' /></Link>
