d65925ba8e48a58b53b54455e0a62abc31188141
Alena Shilimova <ashilimova@mail.ru>
2024-09-24 20:45:01 +0200

    update

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -45,8 +45,8 @@ export interface ILoginFormValues {
   });
     const formik = useFormik({
       initialValues: {
-        username: 'masha',
-        password: 'Masha123!'
+        username: 'admin',
+        password: 'Qwerty123?@'
       } as ILoginFormValues,
       validationSchema: schema,
       validateOnChange: false,
