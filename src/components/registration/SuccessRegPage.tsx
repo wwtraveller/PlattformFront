2a97b92742f90f3fa9ad7b6ab4c3ee42e0762ba4
AlexV-B <vas681@gmail.com>
2024-09-24 21:49:35 +0200

    added About Us and Uniq Username code

---
+++ src/components/registration/SuccessRegPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SuccessRegPage = () => {
+  return (
+    <div>
+      <h2>Регистрация прошла успешно!</h2>
+      <p>Вы можете войти в систему, используя свои учетные данные.</p>
+    </div>
+  );
+};
+
+export default SuccessRegPage;
